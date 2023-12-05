609f0695d27714fc524f4188e9bff28564137ad7
Aryo-Wibisono <yoannes.deo@ti.ukdw.ac.id>
2023-12-05 22:28:55 +0700

    add internet permission, add search component on home

--- BeeScrape app/app/src/main/java/com/dicoding/beescape/screen/page/Home.kt
+++ BeeScrape app/app/src/main/java/com/dicoding/beescape/screen/page/Home.kt
@@ -1,9 +1,77 @@
 package com.dicoding.beescape.screen.page
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.dicoding.beescape.R
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeScreen(){
-    Text(text = "home")
+fun HomeScreen() {
+    Scaffold(topBar = { TopAppBar(title = { Text(text = "Selamat datang user") }) }) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(40.dp)
+        ) {
+            Text(
+                text = "Konten di bawah TopAppBar",
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+
+            Search()
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Search(modifier: Modifier = Modifier) {
+    SearchBar(
+        query = "",
+        onQueryChange = {},
+        onSearch = {},
+        active = false,
+        onActiveChange = {},
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.onSurfaceVariant
+            )
+        },
+        placeholder = {
+            Text(stringResource(R.string.search))
+        },
+        shape = MaterialTheme.shapes.large,
+        colors = SearchBarDefaults.colors(
+            containerColor = MaterialTheme.colorScheme.background
+        ),
+        modifier = modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+            .heightIn(min = 48.dp)
+    ) {
+    }
 }

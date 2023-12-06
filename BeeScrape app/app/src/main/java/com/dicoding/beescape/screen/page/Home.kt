7cb57af19ce32a0ccf86e38ba2e867c0e877a203
Aryo-Wibisono <yoannes.deo@ti.ukdw.ac.id>
2023-12-06 17:30:43 +0700

    add layout home

--- BeeScrape app/app/src/main/java/com/dicoding/beescape/screen/page/Home.kt
+++ BeeScrape app/app/src/main/java/com/dicoding/beescape/screen/page/Home.kt
@@ -1,15 +1,20 @@
 package com.dicoding.beescape.screen.page
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -19,30 +24,48 @@ import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import com.dicoding.beescape.R
+import com.dicoding.component.suggestCategory
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen() {
-    Scaffold(topBar = { TopAppBar(title = { Text(text = "Selamat datang user") }) }) {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(40.dp)
-        ) {
-            Text(
-                text = "Konten di bawah TopAppBar",
-                style = MaterialTheme.typography.bodyMedium,
-                modifier = Modifier.padding(bottom = 16.dp)
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    ) {
+                        Text(text = "Selamat datang")
+                        Text(text = "User123")
+                    }
+                }
             )
-
-            Search()
+        },
+        content = {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(top = 30.dp, start = 10.dp, end = 10.dp)
+            ) {
+                Search()
+                CategoryRow()
+                ItemRow(name = "", photo = "", modifier = Modifier)
+            }
         }
-    }
+    )
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -69,9 +92,76 @@ fun Search(modifier: Modifier = Modifier) {
             containerColor = MaterialTheme.colorScheme.background
         ),
         modifier = modifier
-            .padding(16.dp)
+            .padding(top = 30.dp, start = 0.dp, end = 0.dp)
             .fillMaxWidth()
             .heightIn(min = 48.dp)
     ) {
+    }
+}
+
+@Composable
+fun CategoryItem(
+    category: String,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Button(onClick = {}) {
+            Text(text = stringResource(category.toInt()))
+        }
+    }
+}
+
+@Composable
+fun CategoryRow(
+    modifier: Modifier = Modifier
+) {
+    Column(modifier.padding(top = 20.dp, start = 10.dp, end = 10.dp)) {
+        Text(
+            text = stringResource(R.string.category),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 8.dp),
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp
+        )
+        LazyRow(
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            contentPadding = PaddingValues(horizontal = 16.dp),
+            modifier = modifier
+        ) {
+            items(suggestCategory) { category ->
+                CategoryItem(category.toString())
+            }
+        }
+    }
+}
+
+@Composable
+fun ItemRow(name: String, photo: String, modifier: Modifier) {
+    Column(modifier.padding(top = 20.dp, start = 10.dp, end = 10.dp)) {
+        Text(
+            text = stringResource(R.string.popular),
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp
+        )
+        LazyRow(
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            contentPadding = PaddingValues(horizontal = 16.dp),
+            modifier = modifier
+        ) {
+           items(suggestCategory){
+               AsyncImage(
+                   model = photo, contentDescription = "photo", contentScale = ContentScale.Crop,
+                   modifier = Modifier
+                       .padding(8.dp)
+                       .size(60.dp)
+               )
+               Text(text = name)
+           }
+        }
     }
 }

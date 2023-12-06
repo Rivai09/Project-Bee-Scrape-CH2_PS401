03d2511b72a0138cafaa0b9e69ed9f326be3ebe6
Aryo-Wibisono <yoannes.deo@ti.ukdw.ac.id>
2023-12-06 18:59:02 +0700

    add selectedmarket

--- BeeScrape app/app/src/main/java/com/dicoding/beescape/screen/page/SelectMarketplace.kt
+++ BeeScrape app/app/src/main/java/com/dicoding/beescape/screen/page/SelectMarketplace.kt
@@ -1,9 +1,49 @@
 package com.dicoding.beescape.screen.page
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.dicoding.beescape.R
 import com.dicoding.beescape.screen.Screen
 
 @Composable
-fun SelectMarketplace(){
-
+fun SelectMarketplace() {
+    Box(
+        modifier = Modifier
+            .padding(top = 20.dp, start = 10.dp, end = 10.dp)
+            .fillMaxWidth()
+    ) {
+        Text(
+            text = stringResource(R.string.marketTittle),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = stringResource(R.string.marketSubtitle),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            fontWeight = FontWeight.Medium,
+            fontSize = 14.sp
+        )
+    }
 }
+
+
+

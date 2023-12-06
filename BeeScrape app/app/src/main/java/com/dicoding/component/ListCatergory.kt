37a7e0d95ba7d4735d957595c6931e0db9db1faf
Aryo-Wibisono <yoannes.deo@ti.ukdw.ac.id>
2023-12-06 17:31:25 +0700

    add collection for category

---
+++ BeeScrape app/app/src/main/java/com/dicoding/component/ListCatergory.kt
@@ -0,0 +1,6 @@
+package com.dicoding.component
+
+import androidx.annotation.StringRes
+import com.dicoding.beescape.R
+
+val suggestCategory= listOf(R.string.categoryHp,R.string.categoryPants,R.string.categoryJacket,R.string.categoryLaptop,R.string.categoryShirt,R.string.categoryTablet)

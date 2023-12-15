cb75fd0910e67a4546521cf2990418af9cf71a77
Aryo-Wibisono <yoannes.deo@ti.ukdw.ac.id>
2023-12-15 21:13:09 +0700

    data dari api udah ke get tapi belum tampil pada home

--- BeeScrape app/app/src/main/java/com/dicoding/beescape/view_model/MainViewModel.kt
+++ BeeScrape app/app/src/main/java/com/dicoding/beescape/view_model/MainViewModel.kt
@@ -1,13 +1,15 @@
 package com.dicoding.beescape.view_model
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.dicoding.beescape.api.response.ItemsItem
 import com.dicoding.beescape.data_user.DataUser
 import com.dicoding.beescape.repository.UserRepository
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
 
 class MainViewModel(private val repository: UserRepository) : ViewModel() {
@@ -28,6 +30,8 @@ class MainViewModel(private val repository: UserRepository) : ViewModel() {
 //            }
 //        }
 //    }
+
+    fun getDataPaging(token: String): Flow<PagingData<ItemsItem>> = repository.getDataPaging(token).cachedIn(viewModelScope)
 
     fun logout() {
         viewModelScope.launch {
